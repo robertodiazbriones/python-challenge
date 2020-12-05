155b61bc2955a01641eeef55f8232c9bb7f46f0f
unknown <robertodiazbriones@gmail.com>
2020-12-04 21:37:37 -0500

    PyBank HW Done

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,9 @@
 import csv
 
+#Reading budget data file
 file =list(csv.reader(open("Resources/budget_data.csv", "r")))
 
-print(file[1])
+#Initializing variables
 i=0
 b=0
 c=0
@@ -11,6 +12,7 @@ profit=0
 Greatest_Aux=0
 Greatest_Inc=0
 Greatest_Dec=0
+SumChange=0.0
 last_row=0
 
 for row in file:
@@ -26,12 +28,28 @@ for row in file:
         elif Greatest_Aux < Greatest_Dec:
             Greatest_Dec=Greatest_Aux
             d=i-1
+    SumChange=SumChange+Greatest_Aux
     last_row=row
-        
-print (int(b))
-print(int(profit))
-print(Greatest_Inc)
-print(Greatest_Dec)
-print("Greatest Increase in profits: "+str(file[c][0]))
-print("Greatest Decrease in profits: "+str(file[d][0]))
+AvgChange=SumChange/(b-1)
 
+#Printing results
+print("Financial Analisis")
+print("------------------")
+print("Total Months:"+str(b))
+print("Total: $"+str(profit))
+print("Average Change: $"+str(round(AvgChange,2)))
+print("Greatest Increase in profits: "+str(file[c][0])+" ($"+str(Greatest_Inc)+")")
+print("Greatest Decrease in profits: "+str(file[d][0])+" ($"+str(Greatest_Dec)+")")
+
+#Create Results file to export results
+ResultsFile=open("Resources/Results.txt", "w")
+
+ResultsFile.write("Financial Analisis")
+ResultsFile.write("------------------")
+ResultsFile.write("Total Months:"+str(b)+"\n")
+ResultsFile.write("Total: $"+str(profit)+"\n")
+ResultsFile.write("Average Change: $"+str(round(AvgChange,2))+"\n")
+ResultsFile.write("Greatest Increase in profits: "+str(file[c][0])+" ($"+str(Greatest_Inc)+")\n")
+ResultsFile.write("Greatest Decrease in profits: "+str(file[d][0])+" ($"+str(Greatest_Dec)+")\n")
+
+ResultsFile.close()
